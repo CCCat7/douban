b76c9d95a6c4971b70caf629c82b2b6ed4d13e7d
Tkz <450583042@qq.com>
2019-11-14 23:42:41 +0800

    Movie.java

---
+++ DAO/Movie.java
@@ -0,0 +1,77 @@
+package doubandao;
+
+public class Movie {
+
+
+	private String Movie_ID;
+	private String Movie_Title;
+	private String Writer;
+	private String Country;
+	private String Movie_Date;
+	private String Movie_Time;
+	private String Language;
+	private String Another_Name;
+	private String Movie_Link;
+	private String Movie_Brief;
+	public String getMovie_ID() {
+		return Movie_ID;
+	}
+	public void setMovie_ID(String movie_ID) {
+		Movie_ID = movie_ID;
+	}
+	public String getMovie_Title() {
+		return Movie_Title;
+	}
+	public void setMovie_Title(String movie_Title) {
+		Movie_Title = movie_Title;
+	}
+	public String getWriter() {
+		return Writer;
+	}
+	public void setWriter(String writer) {
+		Writer = writer;
+	}
+	public String getCountry() {
+		return Country;
+	}
+	public void setCountry(String country) {
+		Country = country;
+	}
+	public String getMovie_Date() {
+		return Movie_Date;
+	}
+	public void setMovie_Date(String movie_Date) {
+		Movie_Date = movie_Date;
+	}
+	public String getMovie_Time() {
+		return Movie_Time;
+	}
+	public void setMovie_Time(String movie_Time) {
+		Movie_Time = movie_Time;
+	}
+	public String getLanguage() {
+		return Language;
+	}
+	public void setLanguage(String language) {
+		Language = language;
+	}
+	public String getAnother_Name() {
+		return Another_Name;
+	}
+	public void setAnother_Name(String another_Name) {
+		Another_Name = another_Name;
+	}
+	public String getMovie_Link() {
+		return Movie_Link;
+	}
+	public void setMovie_Link(String movie_Link) {
+		Movie_Link = movie_Link;
+	}
+	public String getMovie_Brief() {
+		return Movie_Brief;
+	}
+	public void setMovie_Brief(String mpvoe_Brief) {
+		Movie_Brief = mpvoe_Brief;
+	}
+
+}
