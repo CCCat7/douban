d86454a1ee4345214e275d63df4f073f4855e699
Tkz <450583042@qq.com>
2019-11-14 23:28:36 +0800

    Collect.java

---
+++ DAO/Collect.java
@@ -0,0 +1,19 @@
+package doubandao;
+
+public class Collect {
+	private String Movie_ID;
+	private String Users_ID;
+	
+	public String getMovie_ID() {
+		return Movie_ID;
+	}
+	public void setMovie_ID(String movie_ID) {
+		Movie_ID = movie_ID;
+	}
+	public String getUsers_ID() {
+		return Users_ID;
+	}
+	public void setUsers_ID(String users_ID) {
+		Users_ID = users_ID;
+	}
+}
