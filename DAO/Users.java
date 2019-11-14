2ccaf46af1e53e80ac59962f92e0c0222d08854f
CCCat7 <357910972@qq.com>
2019-11-14 19:57:01 +0800

    Users.java

---
+++ DAO/Users.java
@@ -0,0 +1,49 @@
+package doubandao;
+
+public class Users {
+	private String Users_ID;
+	private String Account;
+	private String Password;
+	private String Tel;
+	private String Users_Name;
+	private String Account_Type;
+	
+	public String getUsers_ID() {
+		return Users_ID;
+	}
+	public void setUsers_ID(String users_ID) {
+		Users_ID = users_ID;
+	}
+	public String getAccount() {
+		return Account;
+	}
+	public void setAccount(String account) {
+		Account = account;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getTel() {
+		return Tel;
+	}
+	public void setTel(String tel) {
+		Tel = tel;
+	}
+	public String getUsers_Name() {
+		return Users_Name;
+	}
+	public void setUsers_Name(String users_Name) {
+		Users_Name = users_Name;
+	}
+	public String getAccount_Type() {
+		return Account_Type;
+	}
+	public void setAccount_Type(String account_Type) {
+		Account_Type = account_Type;
+	}
+	
+	
+}
