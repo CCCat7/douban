cf6f9a4e28a9004d4c3f92f9a0b80acfa46f7cde
tedfox6 <925361729@qq.com>
2019-11-15 22:24:46 +0800

    Participate_Discuss.java

---
+++ DAO/Participate_Discuss.java
@@ -0,0 +1,25 @@
+package doubandao;
+
+public class Participate_Discuss {
+	private String Discuss_ID;
+	private String Users_ID;
+	private String Discuss_Content;
+	public String getDiscuss_ID() {
+		return Discuss_ID;
+	}
+	public void setDiscuss_ID(String discuss_ID) {
+		Discuss_ID = discuss_ID;
+	}
+	public String getUsers_ID() {
+		return Users_ID;
+	}
+	public void setUsers_ID(String users_ID) {
+		Users_ID = users_ID;
+	}
+	public String getDiscuss_Content() {
+		return Discuss_Content;
+	}
+	public void setDiscuss_Content(String discuss_Content) {
+		Discuss_Content = discuss_Content;
+	}
+}
