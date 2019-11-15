f9b37cf86110492274bd5b1a5b2d3a38fcbf91e6
CCCat7 <357910972@qq.com>
2019-11-15 09:02:41 +0800

    "delete"

--- DAO/Performer_AwardDAO.java
+++
@@ -1,8 +0,0 @@
-package doubandao;
-
-public interface Performer_AwardDAO {
-	public void  insertPerformer_Award(Performer_Award pa);
-	public void  deletePerformer_Award(String award_Name,  String award_Message);
-	public void  updatePerformer_Award(Performer_Award pa);
-	public Performer_Award  Performer_Award(String award_Name,  String award_Message);
-}
--- DAO/Performer_GetAwardDAO.java
+++
@@ -1,8 +0,0 @@
-package doubandao;
-
-public interface Performer_GetAwardDAO {
-	public void  insertPerformer_GetAward(Performer_GetAward pg);
-	public void  deletePerformer_GetAward(String award_Name, String performer_ID);
-	public void  updatePerformer_GetAward(Performer_GetAward pg);
-	public Performer_GetAward  getPerformer_GetAward(String award_Name, String performer_ID);
-}
