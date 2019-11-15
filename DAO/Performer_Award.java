414c35b25f522360e4a77a6b90710c3751ccdd99
CCCat7 <357910972@qq.com>
2019-11-15 09:04:53 +0800

    Performer_Award.java

---
+++ DAO/Performer_Award.java
@@ -0,0 +1,19 @@
+package doubandao;
+
+public class Performer_Award {
+	private String Award_Name;
+	private String Award_Message;
+	public String getAward_Name() {
+		return Award_Name;
+	}
+	public void setAward_Name(String award_Name) {
+		Award_Name = award_Name;
+	}
+	public String getAward_Message() {
+		return Award_Message;
+	}
+	public void setAward_Message(String award_Message) {
+		Award_Message = award_Message;
+	}
+	
+}
