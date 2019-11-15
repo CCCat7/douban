2e4bfc90780ff6720e6c5c9efe0f4d640adee8ef
Tkz <450583042@qq.com>
2019-11-15 21:55:29 +0800

    Group_Discuss.java

---
+++ DAO/Group_Discuss.java
@@ -0,0 +1,41 @@
+package doubandao;
+
+public class Group_Discuss {
+	private String Discuss_ID;
+	private String Movie_ID;
+	private String Users_ID;
+	private String Discuss_Title;
+	private java.sql.Timestamp Discuss_Date;
+	public String getDiscuss_ID() {
+		return Discuss_ID;
+	}
+	public void setDiscuss_ID(String discuss_ID) {
+		Discuss_ID = discuss_ID;
+	}
+	public String getMovie_ID() {
+		return Movie_ID;
+	}
+	public void setMovie_ID(String movie_ID) {
+		Movie_ID = movie_ID;
+	}
+	public String getUsers_ID() {
+		return Users_ID;
+	}
+	public void setUsers_ID(String users_ID) {
+		Users_ID = users_ID;
+	}
+	public String getDiscuss_Title() {
+		return Discuss_Title;
+	}
+	public void setDiscuss_Title(String discuss_Title) {
+		Discuss_Title = discuss_Title;
+	}
+	public java.sql.Timestamp getDiscuss_Date() {
+		return Discuss_Date;
+	}
+	public void setDiscuss_Date(java.sql.Timestamp discuss_Date) {
+		Discuss_Date = discuss_Date;
+	}
+	
+
+}
