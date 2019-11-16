5447074a1726ec665a40e6217a7d14d04291e80e
CCCat7 <357910972@qq.com>
2019-11-16 12:48:54 +0800

    Watch.java

---
+++ DAO/Watch.java
@@ -0,0 +1,18 @@
+package doubandao;
+
+public class Watch {
+	private String Movie_ID;
+	private String Users_ID;
+	public String getMovie_ID() {
+		return Movie_ID;
+	}
+	public void setMovie_ID(String movie_ID) {
+		Movie_ID = movie_ID;
+	}
+	public String getUsers_ID() {
+		return Users_ID;
+	}
+	public void setUsers_ID(String users_ID) {
+		Users_ID = users_ID;
+	}
+}
