c6798d33e5c6ccde367d4b3fd7b408b8cffbed61
Tkz <450583042@qq.com>
2019-11-16 13:04:53 +0800

    Lists.java

---
+++ DAO/Lists.java
@@ -0,0 +1,26 @@
+package doubandao;
+
+public class Lists {
+	
+	private String Movie_ID;
+	private String Label_Content;
+	private String LikeUsers_ID;
+	public String getMovie_ID() {
+		return Movie_ID;
+	}
+	public void setMovie_ID(String movie_ID) {
+		Movie_ID = movie_ID;
+	}
+	public String getLabel_Content() {
+		return Label_Content;
+	}
+	public void setLabel_Content(String label_Content) {
+		Label_Content = label_Content;
+	}
+	public String getLikeUsers_ID() {
+		return LikeUsers_ID;
+	}
+	public void setLikeUsers_ID(String likeUsers_ID) {
+		LikeUsers_ID = likeUsers_ID;
+	}
+}
