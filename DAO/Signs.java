cc073ea3040ffedd56f8f7e8d9a6070bc41a3f77
CCCat7 <357910972@qq.com>
2019-11-16 12:48:09 +0800

    Signs.java

---
+++ DAO/Signs.java
@@ -0,0 +1,18 @@
+package doubandao;
+
+public class Signs {
+	private String Movie_ID;
+	private String Label_Content;
+	public String getMovie_ID() {
+		return Movie_ID;
+	}
+	public void setMovie_ID(String movie_ID) {
+		Movie_ID = movie_ID;
+	}
+	public String getLabel_Content() {
+		return Label_Content;
+	}
+	public void setLabel_Content(String label_Content) {
+		Label_Content = label_Content;
+	}
+}
