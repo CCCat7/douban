5b3a367bceeb7ee000802fbf14e4889721117b9d
Tkz <450583042@qq.com>
2019-11-17 23:23:41 +0800

    Update UsersDAOImp.java

--- DAO/UsersDAOImp.java
+++ DAO/UsersDAOImp.java
@@ -8,6 +8,7 @@ public class UsersDAOImp extends DAOBASE implements UsersDAO {
 
 	private static final String INSERT_USERS_SQL = "INSERT INTO Users VALUES(?, ?, ?, ?, ?, ?)";
 	@Override
+
 	public void insertUsers(Users usr) {
 		// TODO Auto-generated method stub
 		Connection conn = null;
@@ -84,7 +85,7 @@ public class UsersDAOImp extends DAOBASE implements UsersDAO {
 			pst = conn.prepareStatement(GET_USERS_SQL);
 			pst.setString(1, users_ID);
 			rs = pst.executeQuery();
-			
+			rs.next();
 			usr.setAccount(rs.getString("Account"));
 			usr.setAccount_Type(rs.getString("Account_Type"));
 			usr.setPassword(rs.getString("Password"));
@@ -100,5 +101,85 @@ public class UsersDAOImp extends DAOBASE implements UsersDAO {
 		}
 		
 		return usr;
+	}
+	
+	private static final String GET_USERS_LOGIN_SQL = "SELECT Users_ID FROM Users WHERE Account_Type = ? AND Account=? AND Password=?";
+	public int getLogin(String users_type,String users_acco,String users_pwd) {
+		Connection conn = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		Users usr = new Users();
+		int flag=0;
+		try {
+			conn = getConnection();
+			pst = conn.prepareStatement(GET_USERS_LOGIN_SQL);
+			pst.setString(1, users_type);
+			pst.setString(2, users_acco);
+			pst.setString(3, users_pwd);
+			rs = pst.executeQuery();
+			rs.next();
+			usr.setUsers_ID(rs.getString("Users_ID"));
+			if(usr.getUsers_ID().length()!=0)
+			{
+				flag=1;
+			}
+			else
+				flag=0;
+			pst.close();
+			conn.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	
+	private static final String GET_USERSACCOUNT_SQL = "SELECT COUNT(Users_ID) AS X FROM Users";
+
+	public String getUsers_id()
+	{
+		Connection conn = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		Users usr = new Users();
+		String m=null;
+		try {
+			conn = getConnection();
+			pst = conn.prepareStatement(GET_USERSACCOUNT_SQL);
+			rs = pst.executeQuery();
+			rs.next();
+			m=rs.getString("X");
+			pst.close();
+			conn.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return m;
+	}
+	
+	private static final String GET_NOWID_SQL = "SELECT Users_ID FROM Users WHERE Account=?";//获取当前用户id
+
+	public String getUsers_id(String account)
+	{
+		Connection conn = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		Users usr = new Users();
+		String m=null;
+		try {
+			conn = getConnection();
+			pst = conn.prepareStatement(GET_NOWID_SQL);
+			pst.setString(1, account);
+			rs = pst.executeQuery();
+			rs.next();
+			m=rs.getString("Users_ID");
+			pst.close();
+			conn.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return m;
 	}
 }
