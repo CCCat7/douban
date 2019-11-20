ef442d4028c3c24b53a6d1e6c4a40baba728904b
tedfox6 <925361729@qq.com>
2019-11-20 23:13:41 +0800

    Update Performer_GetAwardDAOImp.java

--- DAO/Performer_GetAwardDAOImp.java
+++ DAO/Performer_GetAwardDAOImp.java
@@ -77,7 +77,7 @@ public class Performer_GetAwardDAOImp extends DAOBASE implements Performer_GetAw
 			pst.setString(1, award_Name);
 			pst.setString(2, performer_ID);
 			rs = pst.executeQuery();
-			rs.next(); //-----------------
+			rs.next();
 			pg.setAward_Name(rs.getString("Award_Name"));
 			pg.setPerformer_ID(rs.getString("Performer_ID"));
 			
@@ -89,5 +89,63 @@ public class Performer_GetAwardDAOImp extends DAOBASE implements Performer_GetAw
 		}
 		
 		return pg;
+	}
+	
+	private static final String GET_PERFROMER_NUM_SQL = "SELECT COUNT(Performer_ID) AS X\r\n" + 
+			"FROM Performer_GetAward\r\n" + 
+			"WHERE Award_Name=?";
+	public int GetPerformer_Num(String award_name)//获该奖的演职员数量
+	{
+		Connection conn = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+	    int Performer_Num = 0;
+		try {
+			conn = getConnection();
+			pst = conn.prepareStatement(GET_PERFROMER_NUM_SQL);
+			pst.setString(1, award_name);
+			rs = pst.executeQuery();
+			rs.next();
+			Performer_Num = rs.getInt("X");
+			pst.close();
+			conn.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return Performer_Num;
+	}
+	
+	private static final String GET_PerformerS_SQL = "SELECT Performer_Name\r\n" + 
+			"FROM Performer AS X,Performer_GetAward AS Y\r\n" + 
+			"WHERE X.Performer_ID=Y.Performer_ID AND Y.Award_Name=?";
+	public String GetPerformers_Name(String award_name)//获得该奖的所有演职员
+	{
+		Connection conn = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		int num=GetPerformer_Num(award_name);
+		String [] Performer_Name = new String[num];
+		try {
+			conn = getConnection();
+			pst = conn.prepareStatement(GET_PerformerS_SQL);
+			pst.setString(1, award_name);
+			rs = pst.executeQuery();
+			for(int i=0;i<num;i++)
+			{
+		    rs.next();
+			Performer_Name[i] = rs.getString("Performer_Name");
+			}
+			pst.close();
+			conn.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		String AllPerformer=Performer_Name[0] + "/";
+		for(int i=1;i<num-1;i++)
+		AllPerformer = AllPerformer + Performer_Name[i] + "/";		
+		AllPerformer = AllPerformer + Performer_Name[num-1];
+		return AllPerformer;
 	}
 }
