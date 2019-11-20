77d6ea6131f18602c11114898f6a654993718883
Tkz <450583042@qq.com>
2019-11-20 23:23:18 +0800

    Collection.java

--- Functions/Collection.java
+++ Functions/Collection.java
@@ -13,29 +13,42 @@ public class Collection {
 		System.out.println("请输入要查看的电影名:");
 		String movie_title=reader.nextLine();
 		MovieDAOImp m=new MovieDAOImp();
-		m.GetMovie_Message(m.GetMovie_ID(movie_title));
+		Movie_CollectDAOImp mcd=new Movie_CollectDAOImp();
+		m.GetMovie_Message(mcd.GetCollection_ID(movie_title));
 		
 		
 	}
 
 	public void do_collect(String movie_id,String users_id)//进行收藏,当前电影id和用户id
 	{
-		String temp=null;
-		System.out.println("是否确定收藏此电影		Y/N");
-		temp=reader.nextLine();
+		/*System.out.println("是否确定收藏此电影		Y/N");
+		String temp=reader.nextLine();
 		switch(temp)
 		{
 		case "Y":
-		case "y":
+		case "y":*/
 			Collect col=new Collect();
 			col.setMovie_ID(movie_id);
 			col.setUsers_ID(users_id);
 			
 			CollectDAOImp coldaoimp=new CollectDAOImp();
 			coldaoimp.insertCollect(col);
+		/*	break;
+		case "N":
+		case "n":
+			Login_Success lo=new Login_Success();
+			lo.login_success();*/
 		}
-		
+	
+	public void don_collect(String movie_title,String movie_id)//放到movie_collect表中
+	{
+		Movie_Collect mc=new Movie_Collect();
+		mc.setMovie_ID(movie_id);
+		mc.setMovie_Title(movie_title);
+		Movie_CollectDAOImp mcd=new Movie_CollectDAOImp();
+		mcd.insertMovie_Collect(mc);
 		
 	}
-
 }
+
+
